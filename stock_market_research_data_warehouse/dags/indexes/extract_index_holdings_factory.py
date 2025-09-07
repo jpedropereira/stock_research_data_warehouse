cd00f8dbcb154ddb48409cfcc4dcb72bddf7755c
Joao Pereira <84179710+jpedropereira@users.noreply.github.com>
2025-09-08 00:18:15 +0200

    Apply dag factory design pattern to index holdings extraction

---
+++ stock_market_research_data_warehouse/dags/indexes/extract_index_holdings_factory.py
@@ -0,0 +1,180 @@
+import logging
+from datetime import datetime, timedelta
+from os import path
+from tempfile import NamedTemporaryFile
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from include.config import PROJECT_ROOT, STAGING_SCHEMA, now_tz
+from include.indexes.constants import (
+    BUCKET_INDEXES_ISHARES_HOLDINGS,
+    COL_MAPPINGS,
+    COMMON_ARGS,
+)
+from include.indexes.index_holdings import (
+    get_ishares_etf_holdings,
+    get_ishares_etf_holdings_csv_url,
+)
+from include.utils import render_ddl
+from plugins.dags import BaseDAGFactory
+from plugins.operators import EnforceLatestFileOperator, ExtractToStagingOperator
+
+tables_yaml_path = path.join(PROJECT_ROOT, "include", "raw_files_column_mappings.yaml")
+indexes_yaml_path = path.join(PROJECT_ROOT, "include", "indexes", "indexes.yaml")
+table_name = "index_holdings"
+
+
+# Define the default configuration
+dag_args = {
+    "start_date": datetime(2025, 8, 18, 1),
+    "catchup": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=2),
+}
+
+default_args = {
+    **COMMON_ARGS,
+    **dag_args,
+}
+
+
+class ExtractIndexesHoldingsDAGFactory(BaseDAGFactory):
+
+    def __init__(
+        self, configs_path: str, table_name: str, tables_yaml_path: str, default_args: dict
+    ):
+        self.table_name = table_name
+        self.tables_yaml_path = tables_yaml_path
+        self.default_args = default_args
+        super().__init__(configs_path)
+
+    def create_dag_template(self, dag_id, index, default_args, schedule):
+        dag = DAG(
+            dag_id=dag_id,
+            default_args=default_args,
+            schedule=schedule if schedule else None,
+            tags=["stocks", "data_extraction", "indexes", "etf", f"{index}"],
+        )
+
+        with dag:
+
+            @task()
+            def start_task():
+                return None
+
+            start_task = start_task()
+
+            @task()
+            def end_task():
+                return None
+
+            end_task = end_task()
+
+            @task()
+            def get_ishares_indexes_task():
+                return self.configs[index]["ishares_etf_url"]
+
+            get_ishares_indexes_task = get_ishares_indexes_task()
+
+            @task()
+            def get_ishares_holdings_csv_url(etf_url: str) -> str:
+                logging.info(f"Finding holdings csv url for {index} index.")
+                # Get the ticker from the index configuration for better accuracy
+                url = get_ishares_etf_holdings_csv_url(etf_url)
+                logging.info(f"Holdings csv url for {index} index is {url}")
+                return url
+
+            get_ishares_holdings_csv_urls = get_ishares_holdings_csv_url(get_ishares_indexes_task)
+
+            @task()
+            def get_ishares_holdings_data(csv_url: str) -> str:
+                logging.info(f"Reading holdings url for {index} index from {csv_url}")
+
+                s3_hook = S3Hook(aws_conn_id="minio_conn")
+                data = get_ishares_etf_holdings(csv_url)
+                data["extraction_datetime"] = now_tz()
+
+                timestamp = now_tz().strftime("%Y%m%d_%H%M%S")
+                file_name = f"{timestamp}_{index}_holdings.csv"
+
+                logging.info(
+                    f"Writting {index} holdings in bucket {BUCKET_INDEXES_ISHARES_HOLDINGS} {file_name}"
+                )
+
+                with NamedTemporaryFile(suffix=".csv") as tmp:
+                    data.to_csv(tmp.name, index=False)
+
+                    s3_hook.load_file(
+                        filename=tmp.name,
+                        key=file_name,
+                        bucket_name=BUCKET_INDEXES_ISHARES_HOLDINGS,
+                        replace=True,
+                    )
+
+                    return file_name
+
+            get_ishares_holdings_data = get_ishares_holdings_data(get_ishares_holdings_csv_urls)
+
+            create_table = SQLExecuteQueryOperator(
+                task_id="create_table_if_not_exists",
+                conn_id="datawarehouse_conn",
+                sql=render_ddl(
+                    column_mapping_yaml_path=tables_yaml_path,
+                    table_name=self.table_name,
+                    schema_name=STAGING_SCHEMA,
+                ),
+            )
+
+            load_to_staging = ExtractToStagingOperator(
+                task_id="load_to_staging",
+                s3_conn_id="minio_conn",
+                bucket_name=BUCKET_INDEXES_ISHARES_HOLDINGS,
+                postgres_conn_id="datawarehouse_conn",
+                schema_name=STAGING_SCHEMA,
+                table_name=self.table_name,
+                column_mapping_yaml_path=tables_yaml_path,
+                object_key="{{ ti.xcom_pull(task_ids='get_ishares_holdings_data') }}",
+                add_label_columns={"index": index},
+                col_mappings=COL_MAPPINGS,
+            )
+
+            enforce_latest_files = EnforceLatestFileOperator(
+                task_id="enforce_latest_files",
+                postgres_conn_id="datawarehouse_conn",
+                deduplication_columns=["index", "ticker"],
+                schema_name=STAGING_SCHEMA,
+                table_name=self.table_name,
+            )
+
+            (
+                start_task
+                >> get_ishares_indexes_task
+                >> get_ishares_holdings_csv_urls
+                >> get_ishares_holdings_data
+                >> create_table
+                >> load_to_staging
+                >> enforce_latest_files
+                >> end_task
+            )
+
+            return dag
+
+    def dag_arguments_generator(self):
+        for index in self.configs:
+            yield {
+                "dag_id": f"extract_{index}_holdings",
+                "index": index,
+                "default_args": default_args,
+                "schedule": self.configs.get(index).get("schedule", None),
+            }
+
+
+factory = ExtractIndexesHoldingsDAGFactory(
+    indexes_yaml_path, table_name, tables_yaml_path, default_args
+)
+
+factory.create_dags()
+
+factory.create_master_dag(master_dag_id="extract_index_holdings_master")
---
+++ stock_market_research_data_warehouse/plugins/dags/__init__.py
@@ -0,0 +1,12 @@
+"""Custom Airflow dag pluggins for the stock market research data warehouse.
+This file simplifies the import structure for staging operations.
+You can use:
+from plugins.dags import BaseDAGFactory
+
+instead of:
+from plugins.dags.dag_factory import BaseDAGFactory
+"""
+
+from plugins.dags.dag_factory import BaseDAGFactory
+
+__all__ = ["BaseDAGFactory"]
